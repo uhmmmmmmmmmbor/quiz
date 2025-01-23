30c1a0be125d9239e505198949e10f378ef6601d
uhmmmmmmmmmbor <bor_ferlez@biswashington.org>
2025-01-23 17:43:50 +0000

    dfsvd

--- main.py
+++ main.py
@@ -1,1 +1,97 @@
+question =  "WHat is the capital of france"
+print(question)
 
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "paris":
+    print("Correct!")
+else:
+    print("Ur dumb, the correct answer is paris")
+
+score = 0
+score += 1
+
+print(f"your score is: {score}")
+
+question =  "What is the capital of slovenia"
+print(question)
+
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "ljubljana":
+    print("Correct!")
+    score += 1
+else:
+    print("Ur dumb, the correct answer is ljubljana")
+
+
+
+print(f"your score is: {score}")
+
+
+
+question =  "WHat is the capital of belgium"
+print(question)
+
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "brussels":
+    print("Correct!")
+    score += 1
+    
+else:
+    print("Ur dumb, the correct answer is brussels")
+
+
+print(f"your score is: {score}")
+
+
+question =  "WHat is the capital of norway"
+print(question)
+
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "oslo":
+    print("Correct!")
+    score += 1
+else:
+    print("Ur dumb, the correct answer is oslo")
+
+
+
+print(f"your score is: {score}")
+
+
+question =  "WHat is the capital of italy"
+print(question)
+
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "rome":
+    print("Correct!")
+
+    score += 1
+
+else:
+    print("Ur dumb, the correct answer is rome")
+
+
+
+
+print(f"your score is: {score}")
+
+
+question =  "WHat is the capital of iran"
+print(question)
+
+user_answer = input("your answer:  ")
+
+if user_answer.lower() == "tehran":
+    print("Correct!")
+
+    score += 1
+else:
+    print("Ur dumb, the correct answer is tehran")
+
+
+print(f"your score is: {score}")
